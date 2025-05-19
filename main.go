ef4342cf7405212e205a52f14f2fc0655a130a06
LipeMachado <silvalipe765@gmail.com>
2025-05-19 14:47:06 -0300

    Variaveis e constantes

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	//Pode renomear o pacote colocando atras do "link"...
+	"fmt"
+
 	"github.com/LipeMachado/go-study/meet"
 )
 
 func main() {
 	meet.Say("Hello Lipe!")
 	meet.SayHello()
+
+	//Variaveis
+	//palavra reservada "var" nome da variavel tipo
+	var x int = 10
+	fmt.Println(x)
+
+	//Declaração curta
+	texto := "Olá"
+	texto += "Mundo"
+	fmt.Println(texto)
+
+	//Constantes
+	const PI float64 = 3.1415
+	fmt.Println(PI)
 }
